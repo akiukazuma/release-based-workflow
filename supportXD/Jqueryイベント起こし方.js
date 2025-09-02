a1e32a814e8c22fcd0f8b54d84a76405a44199ed
akiukazuma <kazuma.akiu@acrocrane.com>
2025-09-02 20:44:48 +0900

    git上に資料を設置

---
+++ supportXD/Jqueryイベント起こし方.js
@@ -0,0 +1,48 @@
+$(function() {
+    //例
+    
+    //クリック時
+    $(document).on('click', 'イベントを起こしたい所のclassとかid', function() {
+
+    });
+
+    //ダブルクリック時
+    $(document).on('dbclick', 'イベントを起こしたい所のclassとかid', function() {
+
+    });
+
+    //フォームにある中の要素が変わった時
+    $(document).on('change', 'イベントを起こしたい所のclassとかid', function() {
+
+    });
+
+    //マウスを押した時
+    $(document).on('mousedown', 'イベントを起こしたい所のclassとかid', function() {
+
+    });
+
+    //マウスを押してから、離した時
+    $(document).on('mouseup', 'イベントを起こしたい所のclassとかid', function() {
+
+    });
+
+    //右クリック
+    $(document).on('contextmenu', 'イベントを起こしたい所のclassとかid', function() {
+
+    });
+
+    //マウスが要素の上で移動した時
+    $(document).on('mousemove', 'イベントを起こしたい所のclassとかid', function() {
+
+    });
+
+    //マウスが要素の上にある時
+    $(document).on('mouseover', 'イベントを起こしたい所のclassとかid', function() {
+
+    });
+
+    //マウスが要素の上から離れた時
+    $(document).on('mouseout', 'イベントを起こしたい所のclassとかid', function() {
+
+    });
+});
